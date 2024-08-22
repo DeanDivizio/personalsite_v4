4fc9ff111e08c709419b8382d471ce641ecfffc4
Dean Divizio <dean@deandivizio.com>
2024-08-22 12:14:36 -0400

    fixed gap in bgblur on work page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -13,7 +13,7 @@ export default function Work() {
   let designBody = "I try to be diverse in my design mediums. While web is what I'm most passionate about, I also enjoy all components of brand design; logos, style guides, business cards, etc. <br><br>I have a few examples here but if you'd like to see more detailed examples, you can click the buttons below for detailed write-ups on the design and development of these brands.";
 
   return (
-    <main className="flex min-h-screen flex-col items-center text-center lg:items-start lg:text-left justify-center overflow-x-hidden pb-16">
+    <main className="flex min-h-screen flex-col items-center text-center lg:items-start lg:text-left justify-center overflow-x-hidden pb-16 sm:pb-0">
       <div className="from-darkGlass lg:from-black lg:from-20% to-darkGlass lg:to-glass bg-work-bg w-screen overflow-hidden">
         <div className="min-h-screen flex items-center">
           <div className="p-12 lg:p-32">
@@ -64,13 +64,13 @@ export default function Work() {
         <ParticleBG />
       </div>
       <MediaSection />
-      <div id="design" className="min-h-screen w-screen flex flex-row justify-center items-start overflow-hidden lg:max-h-[900px]" >
+      <div id="design" className="min-h-screen w-screen flex flex-row justify-center items-start overflow-hidden lg:max-h-[600px]" >
         <div className=" h-full w-screen  flex flex-col">
-          <div className="pt-24 sm:pt-32 sm:pb-8 backdrop-blur-sm">
+          <div className="pt-24 sm:pt-32 h-full sm:pb-8 backdrop-blur-sm">
             <h2 className="empTextDiv text-6xl md:text-8xl mb-6 font-semibold text-center text-slate">{`Design`}</h2>
             <h4 className="text-2xl lg:text-4xl font-light mb-8 lg:mb-16 text-center text-blue">{`Print // Branding`}</h4>
           </div>
-          <div className="flex flex-row items-center justify-center backdrop-blur-sm " >
+          <div className="flex flex-row items-center justify-center backdrop-blur-sm h-full  " >
             <div className="grid grid-rows-2 lg:grid-cols-2 items-center md:gap-16 lg:gap-48 px-8 lg:px-36 pb-8 ">
               <div className="order-2 sm:order-1">
                 <h3 className="text-slate text-4xl mb-8 ">{'Beyond Web'}</h3>
